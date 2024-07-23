c9eb795e6ca462d9ee938cf7f5b8b3518dcead96
matthieu88160 <matthieu.vallance@cscfa.fr>
2024-07-23 19:17:45 +0200

    Add initial code for simple factory report

---
+++ src/FicsitNetwork/Reports/SimpleFactoryReport.lua
@@ -0,0 +1,81 @@
+local producers = component.proxy(component.findComponent("TProducer"))
+
+function getMultiplicator(producer)
+    local name = producer:getType().DisplayName
+    
+    if name == "Constructor x4" then
+        return 4
+    elseif name == "Constructor x10" then
+        return 10
+    elseif name == "Constructor x16" then
+        return 16
+    elseif name == "Constructor x64" then
+        return 64
+    elseif name == "Constructor x100" then
+        return 100
+    elseif name == "Assembler x4" then
+        return 4
+    elseif name == "Assembler x10" then
+        return 10
+    elseif name == "Assembler x16" then
+        return 16
+    elseif name == "Assembler x64" then
+        return 64
+    elseif name == "Assembler x100" then
+        return 100
+    elseif name == "Smelter x4" then
+        return 4
+    elseif name == "Smelter x10" then
+        return 10
+    elseif name == "Smelter x16" then
+        return 16
+    elseif name == "Smelter x64" then
+        return 64
+    elseif name == "Smelter x100" then
+        return 100
+    elseif name == "Foundry x4" then
+        return 4
+    elseif name == "Foundry x10" then
+        return 10
+    elseif name == "Foundry x16" then
+        return 16
+    elseif name == "Foundry x64" then
+        return 64
+    elseif name == "Foundry x100" then
+        return 100
+    else
+        return 1
+    end
+end
+
+for i,producer in ipairs(producers) do
+    local producerName = producer:getType().DisplayName
+    local recipe = producer:getRecipe()
+    local perMinute = 60 / recipe.Duration;
+    for i,product in ipairs(recipe:getProducts()) do
+        local resultName = product.Type.Name
+        local maxOutput = product.Amount * perMinute * getMultiplicator(producer)
+        
+        local buildingLabels = component.proxy(component.findComponent("TInfo BScreen " .. "R" .. product.Type.Name:gsub(" +", "")))
+        local buildingName = "Unknown location"
+        for i,buildingLabel in ipairs(buildingLabels) do
+            local name, value = buildingLabel:getPrefabSignData():getTextElements()
+            buildingName = value[1]
+        end
+                
+        print(
+            resultName 
+            .. ": " 
+            .. (maxOutput * producer.Potential) 
+            .. " (" 
+            .. producerName 
+            .. " = " 
+            .. maxOutput 
+            .. " at " 
+            .. (producer.Potential * 100) 
+            .. "% " 
+            .. buildingName 
+            .. ")"
+        )
+    end
+end
